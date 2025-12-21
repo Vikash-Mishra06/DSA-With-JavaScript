18a692ed119de301637bfb80c99bf42f088eff50
Vikash Mishra <mishravikash0612@gmail.com>
2025-12-21 22:04:53 +0530

    Practiced 5 Basics questions Reversing a String, Check Palindrome, Count Vowels in a String, Find Maximum Number in Array, Remove Duplicates from Array.

---
+++ Day1/script.js
@@ -0,0 +1,69 @@
+// Q.1 Reverse a String
+
+    // let str = "Vikash";
+    // let reversed = str.split('').reverse().join('');
+    // console.log(reversed);
+
+    // Output: hsakiV
+
+    // Explaination: I converted the string into an array, reversed it, and joined it back into a string.
+
+// Q.2 Check Palindrome
+
+    // let str = "madam";
+    // let reversed = str.split("").reverse().join("");
+
+    // if (str === reversed) {
+    //   console.log("Is Palindrome");
+    // } else {
+    //   console.log("Is not Palindrome");
+    // }
+
+    // Output: Is Palindrome
+
+    // Explaination: A palindrome reads the same forward and backward, so I compared the string with reverse.
+
+// Q.3 Count Vowels in a String
+
+    // let str = "JavaScript";
+    // let vowels = "aeiou";
+    // let count = 0;
+
+    // for (let char of str && str.toLowerCase()) {
+    //     if (vowels.includes(char)) {
+    //         count++
+    //     }
+    // }
+    // console.log(count);
+
+    // Output: 3
+
+    // Explaination: To check vowels in string i used for loop to go at each letter and i have used toLowerCase to bring all the letter in lower case and then to find if the str letters are match in vowels so it will increase the count with +1
+
+// Q.4 Find Maximum Number in Array
+
+    // let arr = [3, 7, 2, 9];
+    // let max = arr[0];
+
+    // for (let num of arr) {
+    //   if (num > max) {
+    //     max = num;
+    //   }
+    // }
+
+    // console.log(max);
+
+    // Output: 9
+
+    // Explaination: First i have used for loop to go each numbers of Array and if the number is greater then max so max value will equal to num.
+
+// Q.5 Remove Duplicates from Array
+
+    // let arr = [1, 1, 2, 2, 3, 3, 4, 4];
+    // let unique = [...new Set(arr)];
+
+    // console.log(unique);
+
+    // Output: [ 1, 2, 3, 4 ]
+
+    // Explaination: i have used Set method because Set stores only unique values, and spread operator converts it back to an array.
