8a34d5554eccdc46c0422511bdb6aff83598e4a1
Vikash Mishra <mishravikash0612@gmail.com>
2025-12-22 19:54:18 +0530

    Practiced 5 Basic Questions: Star Pattern, Reverse a Number, Count Character Frequency, Fibonacci Series, Check Prime Number.

---
+++ Day2/script.js
@@ -0,0 +1,72 @@
+// Q.1 Star Pattern
+
+    // for (let i = 1; i <=5; i++) {
+    //     console.log('*'.repeat(i))
+    // }
+
+    // Output: *
+            // **
+            // ***
+            // ****
+            // *****
+
+
+// Q.2 Reverse a Number
+
+    // let num = 1234
+    // let reversed = 0
+
+    // while(num > 0) {
+    //     reversed = reversed * 10 + (num % 10);
+    //     num = Math.floor(num/10)
+    // }
+
+    // console.log(reversed)
+
+    // Output: 4321
+
+// Q.3 Count Character Frequency
+
+    // let str = 'program'
+    // let freq = {}
+
+    // for (let char of str) {
+    //     freq[char] = (freq[char] || 0) + 1
+    // }
+
+    // console.log(freq)
+
+    // Output: { p: 1, r: 2, o: 1, g: 1, a: 1, m: 1 }
+
+// Q.4 Fibonacci Series
+
+    // let a = 0, b = 1;
+
+    // for (let i = 0; i < 5; i++) {
+    //     console.log(a);
+    //     let temp = a + b
+    //     a = b
+    //     b = temp
+    // }
+
+    // Output: 0
+            // 1
+            // 1
+            // 2
+            // 3
+
+// Q.5 Check Prime Number
+
+    // let n = 7;
+    // let isPrime = true;
+
+    // for (let i = 2; i < n; i++) {
+    //   if (n % i === 0) {
+    //     isPrime = false;
+    //     break;
+    //   }
+    // }
+
+    // console.log(isPrime);
+
+    // Output: true
